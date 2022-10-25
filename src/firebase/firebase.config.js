fb8ae23eadd673539067689ca97a9e8aaa51da61
fariharaihan <liafariha5@gmail.com>
2022-10-25 09:30:35 +0600

    route setup

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyA_QXzgn51VzvcsCwjApVxbGVoM2GtiYMU",
+    authDomain: "learning-platform-e93ee.firebaseapp.com",
+    projectId: "learning-platform-e93ee",
+    storageBucket: "learning-platform-e93ee.appspot.com",
+    messagingSenderId: "488278992623",
+    appId: "1:488278992623:web:e5a11ca7fbc3cc55375d1a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
